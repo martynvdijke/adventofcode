077f0a0781af918e09e9e24897768ffefc317ff3
Martyn van Dijke <martijnvdijke600@gmail.com>
2024-12-02 22:04:37 +0100

    feat: day2

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def read_input():
     output = []
     with open("input.txt", "r") as file:
@@ -13,23 +14,85 @@ def read_input():
             output.append(level)
     return output
 
+
 def part1():
     test = read_input()
 
     safe = 0
-    max = 0
     for level in test:
         np_array = np.array(level)
-        grad = np.gradient(np_array)
-        test = ((grad > 0).all() and (grad > -2.).all())
-        print("test")
-        # if np.all(grad, where= ):
-        #     print("test")
+        diff = np.ediff1d(np_array)
+        decrease = (diff < 0).sum()
+        increase = (diff > 0).sum()
+        max_decrease = np.min(diff)
+        max_increase = np.max(diff)
+        if decrease == (len(level) - 1) and max_decrease >= -3:
+            safe += 1
+            continue
+        if increase == (len(level) - 1) and max_increase <= 3:
+            safe += 1
+            continue
 
+    print(safe)
 
 
 def part2():
-    pass
+    test = read_input()
+
+    safe = 0
+    for level in test:
+        np_array = np.array(level)
+        diff = np.ediff1d(np_array)
+        
+        decrease = (diff < 0).sum()
+        increase = (diff > 0).sum()
+        max_decrease = np.min(diff)
+        max_increase = np.max(diff)
+        
+        if decrease == (len(level) - 1) and max_decrease >= -3:
+            safe += 1
+            continue
+        if increase == (len(level) - 1) and max_increase <= 3:
+            safe += 1
+            continue
+        
+        if decrease == (len(level) - 2):
+            safe += decrease_func(level)
+            continue
+
+        if increase == (len(level) - 2):
+            safe += increase_func(level)
+            continue
+
+    print(safe)
+
+
+def increase_func(level):
+    diff = np.ediff1d(np.array(level))
+    for i in range(0, len(level)):
+        temp2 = level.copy()
+        temp2.pop(i)
+        np_array = np.array(temp2)
+        diff = np.ediff1d(np_array)
+        max_increase = np.max(diff)
+        increase = (diff > 0).sum()
+        if max_increase <= 3 and increase == (len(level) - 2):
+            return 1
+    return 0
+
+
+def decrease_func(level):
+    diff = np.ediff1d(np.array(level))
+    for i in range(0, len(level)):
+        temp2 = level.copy()
+        temp2.pop(i)
+        np_array = np.array(temp2)
+        diff = np.ediff1d(np_array)
+        max_decrease = np.min(diff)
+        decrease = (diff < 0).sum()
+        if max_decrease >= -3 and decrease == (len(level) - 2):
+            return 1
+    return 0
 
 part1()
 part2()
